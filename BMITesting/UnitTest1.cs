3691749c69620ca354e545d51e8d95f31766d01d
Pawel Sikora <sikora.p@icloud.com>
2022-02-23 14:19:10 +0100

    xUnit BMI test added DivideByZero test 01

---
+++ BMITesting/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace BMITesting
+{
+    public class BMITesting
+    {
+        [Fact]
+        public void AdultBMI_CacluclateBMIMethod_TestHeightZeroNoException()
+        {
+            //arrange
+            testTest.BMIAdult ob = new testTest.BMIAdult();
+            ob.setWzrostCm(0);
+            ob.setWagaKg(0);
+
+            //act
+            var exception = Record.Exception(() => ob.CaclulateBMI());
+
+
+            //assert
+            Assert.IsNotType<DivideByZeroException>(exception);
+        }
+    }
+}
